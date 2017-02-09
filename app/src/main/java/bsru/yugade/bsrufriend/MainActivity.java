3a125d809a53af09ba39e67df3d1c4399eba99e0
Yugaze-Tayanee <o.tayanee@outlook.com>
2017-02-09 15:16:00 +0700

    intent to SingUp

--- app/src/main/java/bsru/yugade/bsrufriend/MainActivity.java
+++ app/src/main/java/bsru/yugade/bsrufriend/MainActivity.java
@@ -1,13 +1,39 @@
 package bsru.yugade.bsrufriend;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    // Explicit การประกาศตัวแปร
+    private Button signInButton, signUpButton;
+    private EditText userEditText, passEditText;
+
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        //Bind Widget คือการ Initial Var กับ View บน XML
+        signInButton = (Button) findViewById(R.id.button);
+        signUpButton = (Button) findViewById(R.id.button2);
+        userEditText = (EditText) findViewById(R.id.editText);
+        passEditText = (EditText) findViewById(R.id.editText2);
+
+
+        //Button Controller
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SignUpActivity.class));
+
+            }
+        });
+
+        }   // Main Method
+
+}   // Main Class
