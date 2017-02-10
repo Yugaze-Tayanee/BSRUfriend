b37c3da0a8de6688399839b1aa0b98b17385bb23
Yugaze-Tayanee <o.tayanee@outlook.com>
2017-02-10 09:45:31 +0700

    set method

--- app/src/main/java/bsru/yugade/bsrufriend/SignUpActivity.java
+++ app/src/main/java/bsru/yugade/bsrufriend/SignUpActivity.java
@@ -2,12 +2,58 @@ package bsru.yugade.bsrufriend;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioGroup;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    //Explicit
+    private EditText nameEditText, userEditText, passEditText;
+    private ImageView imageView;
+    private RadioGroup radioGroup;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
-    }
-}
+
+        //Bind Widget
+        bindWidget();
+
+        //Button Controllr
+        buttonController()
+
+
+
+    }   //Main Method
+
+    private void buttonController() {
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }   //onClick
+        });
+
+    }   //buttonController
+
+
+    private void bindWidget() {
+
+        nameEditText = (EditText) findViewById(R.id.editText3);
+        nameEditText = (EditText) findViewById(R.id.editText4);
+        nameEditText = (EditText) findViewById(R.id.editText5);
+        imageView = (ImageView) findViewById(R.id.imageView4);
+        radioGroup = (RadioGroup) findViewById(R.id.ragAvata);
+        button = (Button) findViewById(R.id.button3);
+
+
+    }   //bindWidget
+
+}   //Main Class
