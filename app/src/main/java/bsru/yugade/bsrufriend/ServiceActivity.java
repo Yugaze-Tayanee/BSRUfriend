6c73298b84e91715be1e60f6edad63749f66f81b
Yugaze-Tayanee <o.tayanee@outlook.com>
2017-02-17 11:00:20 +0700

    Edit Lat Lng to Server

--- app/src/main/java/bsru/yugade/bsrufriend/ServiceActivity.java
+++ app/src/main/java/bsru/yugade/bsrufriend/ServiceActivity.java
@@ -73,6 +73,8 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
         // Doing
         afterResume();
 
+        updateLatLng();
+
 
         //Delay
         if (aBoolean) {
@@ -89,6 +91,21 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
 
 
     }   // myLoop
+
+    private void updateLatLng() {
+
+        try {
+
+            EditLatLng editLatLng = new EditLatLng(ServiceActivity.this, loginStrings[0]);
+            editLatLng.execute(Double.toString(userLatADouble),Double.toString(userLngADouble));
+            boolean b = Boolean.parseBoolean(editLatLng.get());
+            Log.d("17febV2", "Result ==> " + b);
+
+        } catch (Exception e) {
+            Log.d("17febV2", "e update ==> " + e.toString());
+        }
+
+    }
 
     @Override
     protected void onResume() {
